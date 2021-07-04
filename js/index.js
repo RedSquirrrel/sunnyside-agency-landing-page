d854e9b4beafdd5e4657d2ccd9359250dee7896d
Anette <anet1@rocketmail.com>
2021-07-04 19:41:22 +0100

    update

--- js/index.js
+++ js/index.js
@@ -6,3 +6,8 @@ menuBtn.addEventListener('click', () => {
     ? (navUL.style.transform = 'translateX(0)')
     : (navUL.style.transform = 'translateX(120%)');
 });
+
+console.log(
+  '%c I am new here',
+  'color: white; background-color:red; padding: 0.5rem; font-size: 1.2rem'
+);
